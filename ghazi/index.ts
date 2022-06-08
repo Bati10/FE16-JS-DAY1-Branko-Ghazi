f3e77f4c5077e0cfa8c465c42da5f5c93cc90618
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2022-06-08 11:43:02 +0200

    ok all

---
+++ ghazi/index.ts
@@ -0,0 +1,57 @@
+//ex1 1
+
+for(let i=1;i<11;i++){
+
+    console.log(`${i}* 1=${i}` )
+  
+
+
+}
+for(let i=1;i<11;i++){
+
+    console.log(`${i}* 2=${2*i}` )
+  
+
+
+}
+for(let i=9;i<11;i++){
+
+    console.log(`${i}* 2=${10*i}` )
+  
+
+
+}
+//ex2
+let myName:{fname:string,lname:string}={fname:'Ghazi',lname:'Alanzi'};
+document.write(myName.fname+"  "+myName.lname)
+let b=[myName.fname];
+let m=[myName.lname]
+b.forEach((pri)=>{for(let i=1;i<11;i++){
+
+    console.log(`${i+pri}` )}})
+function late():any{
+m.forEach((pri)=>{for(let i=1;i<11;i++){
+
+    console.log(`${i+pri}` )}})}
+
+    setTimeout(late,5000);
+
+    //ex3
+    let myArr:string[]=["jon","ted","willi","titan"]
+    myArr.forEach((x, index)=>{
+        console.log(index); 
+        
+        
+    });
+    myArr.forEach((x, index)=>{
+        console.log(x); 
+        
+        
+    });
+    myArr.forEach((x, index)=>{
+        console.log(x+" "+index); 
+        
+        
+    });
+    
+
